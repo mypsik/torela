a7b8f38f353239048966d43b0c49c584c27c0a0b
Anton Keks <anton@azib.net>
2019-04-01 20:50:26 +0300

    store stuff to mongodb

--- _api/index.ts
+++ _api/index.ts
@@ -1,20 +1,26 @@
-import * as express from 'express';
-import * as morgan from 'morgan';
-import config from './config';
+import * as express from 'express'
+import * as morgan from 'morgan'
+import {MongoClient} from 'mongodb'
+import config from './config'
 
-const app = express();
+const app = express()
+app.use(express.json())
+const logger = morgan('[:date] :method :url :status :res[content-length] - :response-time ms')
+app.use(logger)
 
-const logger = morgan('[:date] :method :url :status :res[content-length] - :response-time ms');
-app.use(logger);
+const mongoClient = new MongoClient('mongodb://172.22.0.2:27017', {auth: {user: 'torela', password: 't0relas3cret'}})
+mongoClient.connect().then(() => {
+  const db = mongoClient.db('torela')
 
-app.get('/api/booked', (req, res) => {
-  res.json({
-    '2019-04-04 10:00': {},
-    '2019-04-07 14:00': {},
-    '2019-04-07 18:00': {}
+  app.get('/api/bookings', (req, res) => {
+    db.collection('bookings').find().toArray().then(bookings => res.json(bookings))
   })
-});
 
-app.use(express.static('../_site'));
+  app.post('/api/bookings', (req, res) => {
+    return db.collection('bookings').insertOne(req.body).then(result => res.send(result.insertedId))
+  })
 
-app.listen(config.port, () => console.log(`Listening on port ${config.port}`));
+  app.use(express.static('../_site'))
+
+  app.listen(config.port, () => console.log(`Listening on port ${config.port}`))
+})
