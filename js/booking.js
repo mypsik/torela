36452a6fed8ed71cfbe4dd6a9a9dc49a1f7f0187
Anton Keks <anton@azib.net>
2019-04-02 22:22:55 +0300

    class fields are not yet supported by most browsers

--- js/booking.js
+++ js/booking.js
@@ -2,7 +2,7 @@ class BookingDialog {
   constructor(selector, api, lang) {
     this.api = api
     this.lang = lang
-    this.msg = this.messages[lang]
+    this.msg = bookingMessages[lang]
     this.dialog = $(selector)
 
     this.dialog.find('.close').on('click', this.close.bind(this))
@@ -48,58 +48,58 @@ class BookingDialog {
     alert(this.msg.success)
     location.reload()
   }
+}
 
-  messages = {
-    en: {
-      book: 'Book',
-      childName: 'Child\'s name',
-      childAge: 'Child\'s age',
-      parentName: 'Parent\'s name',
-      email: 'Email',
-      phone: 'Phone',
-      terms: 'Agree with <a href="/en/rules/" target="_blank">house rules</a>',
-      comments: 'Comments',
-      success: 'Thank you for booking! Please pay the booking fee to our bank account',
-      services: '<a href="/en/services/" target="_blank">Additional services</a>',
-      photographer: 'Photographer',
-      videographer: 'Video',
-      facePainting: 'Face painting',
-      icecreamMachine: 'Icecream machine',
-      cleaning: 'Cleaning'
-    },
-    et: {
-      book: 'Broneeri',
-      childName: 'Lapse eesnimi',
-      childAge: 'Lapse vanus',
-      parentName: 'Lapsevanema täisnimi',
-      email: 'E-post',
-      phone: 'Telefon',
-      terms: 'Olen nõus mängutoa <a href="/kodukord/" target="_blank">kodukorraga</a>',
-      success: 'Täname broneeringu eest, ootame teid külla! Palun tasuge broneerimistasu meie kontole',
-      comments: 'Lisainfo',
-      services: '<a href="/lisateenused/" target="_blank">Lisateenused</a>',
-      photographer: 'Fotograaf',
-      videographer: 'Videograaf (3h/300€)',
-      facePainting: 'Näomaalingud (1,5h/50€)',
-      icecreamMachine: 'Jäätise masin (59€+km)',
-      cleaning: 'Koristus (25€)'
-    },
-    ru: {
-      book: 'Бронировать',
-      childName: 'Имя ребёнка',
-      childAge: 'Возраст ребёнка',
-      parentName: 'Полное имя родителя',
-      email: 'E-mail',
-      phone: 'Телефон',
-      terms: 'Соглашаюсь соблюдать <a href="/en/rules/" target="_blank">правила игровой комнаты</a>',
-      success: 'Спасибо за бронировку, ждём вас в гости! Не забудьте перевести предоплату на наш счёт',
-      comments: 'Комментарии',
-      services: '<a href="/ru/services/" target="_blank">Дополнительные услуги</a>',
-      photographer: 'Фотограф',
-      videographer: 'Видеограф',
-      facePainting: 'Крашенье лица',
-      icecreamMachine: 'Машина для изготовления мороженного',
-      cleaning: 'Уборка'
-    }
+const bookingMessages = {
+  en: {
+    book: 'Book',
+    childName: 'Child\'s name',
+    childAge: 'Child\'s age',
+    parentName: 'Parent\'s name',
+    email: 'Email',
+    phone: 'Phone',
+    terms: 'Agree with <a href="/en/rules/" target="_blank">house rules</a>',
+    comments: 'Comments',
+    success: 'Thank you for booking! Please pay the booking fee to our bank account',
+    services: '<a href="/en/services/" target="_blank">Additional services</a>',
+    photographer: 'Photographer',
+    videographer: 'Video',
+    facePainting: 'Face painting',
+    icecreamMachine: 'Icecream machine',
+    cleaning: 'Cleaning'
+  },
+  et: {
+    book: 'Broneeri',
+    childName: 'Lapse eesnimi',
+    childAge: 'Lapse vanus',
+    parentName: 'Lapsevanema täisnimi',
+    email: 'E-post',
+    phone: 'Telefon',
+    terms: 'Olen nõus mängutoa <a href="/kodukord/" target="_blank">kodukorraga</a>',
+    success: 'Täname broneeringu eest, ootame teid külla! Palun tasuge broneerimistasu meie kontole',
+    comments: 'Lisainfo',
+    services: '<a href="/lisateenused/" target="_blank">Lisateenused</a>',
+    photographer: 'Fotograaf',
+    videographer: 'Videograaf (3h/300€)',
+    facePainting: 'Näomaalingud (1,5h/50€)',
+    icecreamMachine: 'Jäätise masin (59€+km)',
+    cleaning: 'Koristus (25€)'
+  },
+  ru: {
+    book: 'Бронировать',
+    childName: 'Имя ребёнка',
+    childAge: 'Возраст ребёнка',
+    parentName: 'Полное имя родителя',
+    email: 'E-mail',
+    phone: 'Телефон',
+    terms: 'Соглашаюсь соблюдать <a href="/en/rules/" target="_blank">правила игровой комнаты</a>',
+    success: 'Спасибо за бронировку, ждём вас в гости! Не забудьте перевести предоплату на наш счёт',
+    comments: 'Комментарии',
+    services: '<a href="/ru/services/" target="_blank">Дополнительные услуги</a>',
+    photographer: 'Фотограф',
+    videographer: 'Видеограф',
+    facePainting: 'Крашенье лица',
+    icecreamMachine: 'Машина для изготовления мороженного',
+    cleaning: 'Уборка'
   }
 }
