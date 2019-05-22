26aee7d6e0bc8471378f35bd1427d0e078b8ca04
Anton Keks <anton@azib.net>
2019-05-22 23:50:02 +0300

    hover causes too much jumping

--- _api/admin/views/styles.ts
+++ _api/admin/views/styles.ts
@@ -1,7 +1,5 @@
 export const styles = `<style>
   th, td { text-align: left; vertical-align: top; padding: 5px; }
   tbody tr:hover { background-color: #f5f5f5; }
-  td > button { display: none; }
-  td:hover > button { display: inline-block; }
   .public { background-color: rgba(255,226,74,0.49); }
 </style>`
