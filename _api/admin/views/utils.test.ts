a5f644d248561cf0ced1e6903cc2f942e6bf2411
Anton Keks <anton@azib.net>
2019-05-22 21:32:15 +0300

    add some tests

---
+++ _api/admin/views/utils.test.ts
@@ -0,0 +1,10 @@
+import {e, today} from './utils'
+
+it('today', () => {
+  expect(today()).toMatch(/^20\d\d-\d\d-\d\d$/)
+})
+
+it('escapes', () => {
+  expect(e('<a>')).toBe('&lt;a>')
+  expect(e('\'hello"')).toBe('&apos;hello&quot;')
+})
