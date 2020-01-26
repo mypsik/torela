bd4aee9b3f0afd36921038ebcf0083401c6b0043
Anton Keks <anton@azib.net>
2020-01-26 14:36:37 +0200

    preserve also stderr

--- _api/redeploy.sh
+++ _api/redeploy.sh
@@ -1,3 +1,3 @@
 mkdir -p logs
-docker logs torela_torela_1 > logs/torela-"$(date +%Y-%m-%d_%H:%m:%S)".log
+docker logs torela_torela_1 >logs/torela-"$(date +%Y-%m-%d_%H:%m:%S)".log 2>&1
 docker-compose -p torela up -d --build
