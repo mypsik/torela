872708df2d81223baf916c803779cf392d1eb129
Anton Keks <anton@azib.net>
2020-01-26 14:37:53 +0200

    gzip logs

--- _api/redeploy.sh
+++ _api/redeploy.sh
@@ -1,3 +1,3 @@
 mkdir -p logs
-docker logs torela_torela_1 >logs/torela-"$(date +%Y-%m-%d_%H:%m:%S)".log 2>&1
+docker logs torela_torela_1 2>&1 | gzip > logs/torela-"$(date +%Y-%m-%d_%H:%m:%S)".log.gz
 docker-compose -p torela up -d --build
